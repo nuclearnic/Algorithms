de45eee5d5564ffc9a0c2696eb52ac7c3ece22a0
nuclearnic <nuclearnicdev@gmail.com>
2019-10-27 18:33:34 +0200

    Complete convex hull

--- ConvexHull/sketch.js
+++ ConvexHull/sketch.js
@@ -1,48 +1,77 @@
-// fswatch -o ~/path/to/watch | xargs -n1 -I {} osascript -e 'tell application "Google Chrome" to tell the active tab of its first window to reload'
-
-const points = []
 const hull = []
-const canvasSize = 600
-const canvasEdge = 50
-const numberOfPoints = 50
+const points = []
+const numberOfPoints = 30
+const buffer = 20
+const canvasSize = 500
 
-let leftMost
-let currentVertex
-let nextVertex
 let index
+let nextIndex
+let leftMostPoint
+let currentVertex
 
 function setup() {
   createCanvas(canvasSize, canvasSize)
-  for(let i=0; i<numberOfPoints; i++) {
-    points.push(createVector(random(canvasEdge, canvasSize - canvasEdge), (random(canvasEdge, canvasSize - canvasEdge))))
+  for (let i = 0; i < numberOfPoints; i++) {
+    points.push(createVector(random(buffer, canvasSize - buffer), random(buffer, canvasSize - buffer)))
   }
   points.sort((a, b) => a.x - b.x)
-  leftMost = points[0]
-  currentVertex = leftMost
-  nextVertex = points[1]    // this is a guess
-  index = 2                 // finding the actual nextVertex will start here
+  leftMostPoint = points[0]  
+  hull.push(leftMostPoint)
+  currentVertex = leftMostPoint
+  nextVertex = points[1]
+  index = 2
 }
 
-
 function draw() {
-  background(0);
-  
-  stroke(255);
-  strokeWeight(5);
-  for(let p of points) {
-    point(p.x, p.y);
+  background(0)
+
+  stroke(255)
+  strokeWeight(8)
+  for (let p of points) {
+    point(p.x, p.y)  
   }
 
-  stroke(0, 255, 0)
-  strokeWeight(30)
-  point(leftMost.x, leftMost.y)
+  stroke(0, 0, 255)
+  fill(0, 0, 255, 50)
+  beginShape()
+  for (let p of hull) {
+    vertex(p.x, p.y)
+  }
+  endShape(CLOSE)
 
   stroke(0, 255, 0)
-  strokeWeight(30)
-  point(currentVertex.x, currentVertex.y)
-  
-  stroke(255, 0, 0)
-  strokeWeight(5)
+  strokeWeight(20)
+  point(leftMostPoint.x, leftMostPoint.y)
+
+  stroke(0, 255, 0) 
+  strokeWeight(2)
   line(currentVertex.x, currentVertex.y, nextVertex.x, nextVertex.y)
 
+  let checking = points[index]
+
+  stroke(255)
+  strokeWeight(2)
+  line(currentVertex.x, currentVertex.y, checking.x, checking.y)
+
+  const a = p5.Vector.sub(nextVertex, currentVertex)
+  const b = p5.Vector.sub(checking, currentVertex)
+  const cross = a.cross(b)
+
+  // if true then checking is further rotated than next
+  if (cross.z < 0) {
+    nextVertex = checking
+  }
+
+  index = index + 1
+  if (index == points.length) {
+    if (nextVertex == leftMostPoint) {
+      console.log('Done')
+      noLoop()
+    } else {
+      hull.push(nextVertex)
+      currentVertex = nextVertex
+      nextVertex = leftMostPoint
+      index = 0
+    }
+  }
 }
--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-# Gift Wrapping Algorithm (Convex Hull)
+# Algorithms
 
-<i>Identify the convex hull, for a given set of points on a 2D plane.</i>
-
-### Usage:
- * To run, open index.html in your browser.
- * For hot reloading, install `fswatch` and then run the following command:
-```
-fswatch -o ~/path/to/watch | xargs -n1 -I {} osascript -e 'tell application "Google Chrome" to tell the active tab of its first window to reload'
-```
-* You can also visit https://editor.p5js.org/ and paste the code in sketch.js into the code block.
+* [Convex Hull](ConvexHull/)
